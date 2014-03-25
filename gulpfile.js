3f73d64978fccdee09d9fac4133983d07a430ef5
Cameron Spear <cameronspear@gmail.com>
2014-03-24 18:47:22 -0700

    Bump to v1.0.2.

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sass   = require('gulp-sass');
 var bump   = require('gulp-bump');
-
-gulp.task('bump', function(){
-  gulp.src('./bower.json')
-    .pipe(bump())
-    .pipe(gulp.dest('./'));
-});
+var git    = require('gulp-git');
 
 gulp.task('styles', function () {
   gulp.src('./scss/*.scss')
@@ -17,7 +12,7 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   gulp.src('./js')
     .pipe(concat('postman.min.js'))
     .pipe(uglify())
@@ -25,3 +20,29 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('default', ['styles', 'scripts']);
+
+
+gulp.task('bump', function () {
+  return gulp.src('./bower.json')
+    .pipe(bump())
+    .pipe(gulp.dest('./'));
+});
+
+// experimenting...
+// gulp.task('add', ['bump'], function () {
+//   return gulp.src('./*')
+//     .pipe(git.add());
+// });
+
+// gulp.task('commit', ['bump', 'add'], function () {
+//   return gulp.src('./*')
+//     .pipe(git.commit('Bump version.'));
+// });
+
+// gulp.task('tag', ['bump', 'add', 'commit'], function (done) {
+//   git.tag('v1.0.3', 'v1.0.3', {}. done);
+// });
+
+// gulp.task('push', ['bump', 'add', 'commit', 'tag'], function () {
+//   git.push('origin', 'master', { args: ' --tags' });
+// });
